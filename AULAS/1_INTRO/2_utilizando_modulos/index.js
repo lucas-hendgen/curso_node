418333f7f4d5fea91924c7563a9d60fee0fcf4cb
Lucas_voxy <srslucas5@gmail.com>
2025-02-25 18:00:18 -0300

    identado

--- AULAS/1_INTRO/2_utilizando_modulos/index.js
+++ AULAS/1_INTRO/2_utilizando_modulos/index.js
@@ -1,10 +1,10 @@
-const fs = require('fs') //file system
+const fs = require("fs"); //file system
 
-fs.readFile('arquivo.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    
-    console.log(data)
+fs.readFile("arquivo.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data);
 });
